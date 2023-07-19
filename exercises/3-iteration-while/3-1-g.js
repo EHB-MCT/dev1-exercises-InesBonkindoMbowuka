f988ae931210f28161e1ce1b6e55d5eaafe73fe5
Ines Bonkindo <ines.bonkindo.mbowuka@student.ehb.be>
2023-07-19 11:32:46 +0200

    3-1-g done

--- exercises/3-iteration-while/3-1-g.js
+++ exercises/3-iteration-while/3-1-g.js
@@ -5,21 +5,24 @@ import * as Utils from "../../scripts/utils.js";
 draw();
 
 function draw() {
-    context.lineWidth = 2;
-    context.strokeStyle = "white";
-    context.fillStyle = "#3498DB";
-    context.fillRect(50, 50, 300, 300);
-    drawConcentricCircles();
+	context.lineWidth = 2;
+	context.strokeStyle = "white";
+	context.fillStyle = "#3498DB";
+	context.fillRect(50, 50, 300, 300);
+	drawConcentricCircles();
 }
 
 function drawConcentricCircles() {
-    let i = ;
-    while () {
-        //This strokeCircle function does not exist yet!
-        //Add it to the utils script:
-        //The function draws the stroke of a circle 
-        //and has the following parameters: x,y,radius
-        Utils.strokeCircle(, , );
-        i = ;
-    }
+	let i = 1;
+	let x = 200;
+	let y = 200;
+	let r = 20;
+	while (i < 11) {
+		//This strokeCircle function does not exist yet!
+		//Add it to the utils script:
+		//The function draws the stroke of a circle
+		//and has the following parameters: x,y,radius
+		Utils.strokeCircle(x, y, r + i * 13);
+		i = i + 1;
+	}
 }
