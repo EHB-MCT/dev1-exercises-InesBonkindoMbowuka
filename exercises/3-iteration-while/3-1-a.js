141e7b65369d47845ec1cd285653233e73e72f31
Ines Bonkindo <ines.bonkindo.mbowuka@student.ehb.be>
2023-07-19 11:15:22 +0200

    3-1 done

--- exercises/3-iteration-while/3-1-a.js
+++ exercises/3-iteration-while/3-1-a.js
@@ -2,7 +2,7 @@
 import context from "../../scripts/context.js";
 import * as Utils from "../../scripts/utils.js";
 
-draw();
+/*draw();
 
 function draw() {
 	context.lineWidth = 2;
@@ -21,4 +21,29 @@ function draw() {
 
 function drawLines(x1, y1, x2, y2) {
 	Utils.drawLine(x1, y1, x2, y2);
+}*/
+
+draw();
+
+function draw() {
+	context.lineWidth = 2;
+	context.strokeStyle = "white";
+	context.fillStyle = "#3498DB";
+	context.fillRect(50, 50, 300, 300);
+
+	let i = 1;
+
+	while (i < 12) {
+		drawLines(75, 50 + i * 25, 325, 50 + i * 25);
+		console.log(i);
+		i++;
+	}
+}
+
+function drawLines(x, y, x2, y2) {
+	context.beginPath();
+
+	context.moveTo(x, y);
+	context.lineTo(x2, y2);
+	context.stroke();
 }
