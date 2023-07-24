63bab51973021acf63ef760d9e00e1b17ec656e6
Ines Bonkindo <ines.bonkindo.mbowuka@student.ehb.be>
2023-07-24 10:41:14 +0200

    6-1 done

--- exercises/6-animation/6-1.js
+++ exercises/6-animation/6-1.js
@@ -5,21 +5,19 @@ import * as Utils from "../../scripts/utils.js";
 let width = context.canvas.width;
 let height = context.canvas.height;
 
+window.onmousemove = move;
 
+function move(eventData) {
+	let x = eventData.pageX;
+	let y = eventData.pageY;
 
+	context.fillStyle = "white";
+	context.beginPath();
+	context.rect(0, 0, width, height);
+	context.fill();
 
-window.onmousemove=drawCrosshair;
-
-
-function drawCrosshair(eventData){
-    context.fillStyle="white";
-    context.fillRect(0,0, width,height);
-    Utils.drawLine(eventData.x, 0, eventData.x, height)
-    Utils.drawLine(0, eventData.y, width, eventData.y)
-
+	context.strokeStyle = "red";
+	context.lineWidth = 5;
+	Utils.drawLine(x, 0, x, height);
+	Utils.drawLine(0, y, width, y);
 }
-
-
-
-
-
